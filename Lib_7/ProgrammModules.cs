f7c166c0af09b79e734c3d480ca3b3177d113020
ИСП-31 <-31@LAB24-07>
2021-12-09 14:53:37 +0300

    Добавьте файлы проекта.

---
+++ Lib_7/ProgrammModules.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lib_7
+{
+    //Название класса без класса
+    public class ProgrammModules
+    {
+        //Изменить название метода
+        //Изменить название аргументов
+        public static void SumHalfNumbers(int inputValue, out int result, out string numberList)
+        {
+            numberList = "";
+            result = 0;
+            //Сокращения и аббровеатуры недопустимы
+            Random randomNumber = new Random();
+            for (int i = 0; i < inputValue; i++)
+            {
+                int randNumber = randomNumber.Next(0, inputValue/2);
+                result += randNumber;
+                numberList += randNumber.ToString() + " ";
+            }
+        }
+    }
+}
